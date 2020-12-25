87b00b7e2b15fcadf2d791b2031ba49d6c40f514
phatnguyen02 <ngphat1211@gmail.com>
2020-12-25 13:42:03 +0700

    update

---
+++ src/baiThucHanh2CoDung/bai1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void input(int &n, int a[]);
+void output(int n, int a[]);
+int main(){
+    int a[100];
+    int n;
+    input(n, a);
+    output(n, a);
+    return 0;
+}
+void input(int &n, int a[]){
+    printf("enter n: ");
+    scanf("%d", &n);
+    for(int i = 1; i<=n; i++){
+        printf("enter the number a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+}
+void output(int n, int a[]){
+      for(int i = 1; i<=n; i++){
+        printf("%4d", a[i]);
+        if(i % 10 == 0){
+            printf("\n");
+        }
+    }
+}
