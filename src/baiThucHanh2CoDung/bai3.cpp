da264bc6f991fb867dad0cc5022de623a7e99840
phatnguyen02 <ngphat1211@gmail.com>
2020-12-25 13:59:45 +0700

    update

--- src/baiThucHanh2CoDung/bai3.cpp
+++ src/baiThucHanh2CoDung/bai3.cpp
@@ -1,27 +1,28 @@
 #include<stdio.h>
 #define Max 100
-void input(int &n, int a[]);
-void ascending(int n, int a[]);
-void output(int n, int a[]);
+void input(int &n, float a[]);
+void ascending(int n, float a[]);
+void output(int n, float a[]);
 int main(){
-    int n, a[Max];
+    int n;
+    float a[Max];
     input(n, a);
     ascending(n, a);
     output(n, a);
 }
-void input(int &n, int a[]){
+void input(int &n, float a[]){
     printf("Enter n: ");
     scanf("%d", &n);
     for(int i=0;i<n;i++){
         printf("\nEnter a[%d]: ", i+1);
-        scanf("%d", &a[i]);
+        scanf("%f", &a[i]);
     }
 }
-void ascending(int n, int a[]){
+void ascending(int n, float a[]){
     int temp;
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
-            if(a[i]>a[j]){
+            if(a[i]<a[j]){
                 temp=a[i];
                 a[i]=a[j];
                 a[j]=temp;
@@ -29,9 +30,10 @@ void ascending(int n, int a[]){
         }
     }
 }
-void output(int n, int a[]){
-    printf("\nAscending number: ");
+void output(int n, float a[]){
+    printf("\nAscending number: 5
+    ");
     for(int i=0;i<n;i++){
-        printf("%4d", a[i]);
+        printf("\t%.2f", a[i]);
     }
 }
