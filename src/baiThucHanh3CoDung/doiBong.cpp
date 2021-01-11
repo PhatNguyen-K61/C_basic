0c64b3012e78e5d43a1a6fe2b2a801a510247a2d
phatnguyen02 <ngphat1211@gmail.com>
2021-01-11 17:08:58 +0700

    update

--- src/baiThucHanh3CoDung/doiBong.cpp
+++ src/baiThucHanh3CoDung/doiBong.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<conio.h>
 #define MAX 100
 struct Team{
     char name[30];
@@ -9,6 +10,8 @@ void inputTeam(Team &team);
 void inputNTeam(Team a[], int &n);
 void outputTeam(Team team);
 void outputNTeam(Team a[], int n);
+void threeTeamsHaveHighestScore(Team a[], int n);
+void output3Team(Team a[]);
 int main(){
     int n;
     Team a[MAX], team;
@@ -16,6 +19,8 @@ int main(){
     scanf("%d", &n);
     inputNTeam(a, n);
     outputNTeam(a, n);
+    threeTeamsHaveHighestScore(a, n);
+    output3Team(a);
 }
 void inputTeam(Team &team)
 {
@@ -42,6 +47,26 @@ void outputTeam(Team team){
 }
 void outputNTeam(Team a[], int n){
     for (int i = 0; i < n; i++)
+    {
+        printf("\nInformation of team %d: ", i + 1);
+        outputTeam(a[i]);
+    }
+}
+void threeTeamsHaveHighestScore(Team a[], int n){
+    //3 đội có số điểm cao nhất
+    Team temp;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i].mark<a[j].mark){
+                temp.mark=a[i].mark;
+                a[i].mark=a[j].mark;
+                a[j].mark=temp.mark;
+            }
+        }
+    }
+}
+void output3Team(Team a[]){
+    for (int i = 0; i < 3; i++)
     {
         printf("\nInformation of team %d: ", i + 1);
         outputTeam(a[i]);
