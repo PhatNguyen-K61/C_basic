73ff1773e79fa2df2f7b75bf5ee8310cb42e8655
phatnguyen02 <ngphat1211@gmail.com>
2021-03-13 01:50:49 +0700

    tạo danh sách students, sử dụng CẤU TRÚC DỮ LIỆU VECTOR. viết hàm add 1 phần tử vào trong students

--- src/oop/Student.cpp
+++ src/oop/Student.cpp
@@ -1,4 +1,6 @@
+#include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 class DateTime{
     private:
@@ -9,13 +11,17 @@ class DateTime{
         DateTime(){
             this->day = 1;
             this->month =1;
-            this->year =1990;
+            this->year =1;
         }
         DateTime(int day,int month,int year){
             this->day = day;
             this->month = month;
             this->year = year;
         }
+        friend ostream &operator<<(ostream &output,const DateTime dateTime){
+            cout<<dateTime.day<<"/"<<dateTime.month<<"/"<<dateTime.year;
+        }
+
 };
 class Student{
     private:
@@ -28,10 +34,38 @@ class Student{
             this->name = name;
             this->birthday = birthday;
         }
-        
+        void print(){
+            cout<<"{id:"<<id<<"name:"<<name<<"birthday:"<<birthday<<"}";
+        }     
+
+};
+class Students{
+    private:
+        vector<Student> students;
+        int count;
+    public:
+        Students(){
+            students = vector<Student>();
+            count = 0;
+        }
+        void add(Student student){
+                this->students.push_back(student);
+                count++;
+        }
+        void print(){
+            for (auto student = students.begin(); student != students.end(); ++student) {
+                student->print();
+                cout<<endl;
+            }
+        }
 };
 int main(){
     DateTime birthday = DateTime(3,2,2013);
-    Student student = Student("001","Minh",birthday);
+    Student student1 = Student("001","Minh",birthday);
+    Student student2 = Student("002","Anh",birthday);
+    Students students = Students();
+    students.add(student1);
+    students.add(student2);
+    students.print();
     return 0;
 }
