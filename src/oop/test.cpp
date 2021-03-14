b39877f7727bda9c48560b5bfd28bcb9e84480fc
phatnguyen02 <ngphat1211@gmail.com>
2021-03-14 22:00:15 +0700

    fix

--- src/oop/test.cpp
+++ src/oop/test.cpp
@@ -9,14 +9,13 @@ struct SinhVien {
 void enter (SinhVien *sinhvien);
 void enter (SinhVien *sinhvien, int &sosinhvien);
 void sapxep (SinhVien *sinhvien,int sosinhvien);
-void print (SinhVien *sinhvien);
 
 int main (){
     SinhVien *sinhvien;
     int sosinhvien;
     sinhvien = (SinhVien *) malloc(sizeof(SinhVien));
     enter(sinhvien,sosinhvien);
-    sapxep (sinhvien,sosinhvien);
+    sapxep(sinhvien,sosinhvien);
     return 0;
 }
 void enter (SinhVien *sinhvien){
@@ -31,32 +30,31 @@ void enter (SinhVien *sinhvien){
 void enter (SinhVien *sinhvien, int &sosinhvien){
     printf ("Nhap So Luong Sinh Vien: ");
     scanf("%d",&sosinhvien);
-    sinhvien = (SinhVien*)realloc(sinhvien,(sosinhvien)*sizeof(SinhVien));
     for (int i = 0; i < sosinhvien; i++){
         printf ("\n--------------------------------");
         printf ("\nNhap thong tin Sinh Vien %d\n",i+1);
-        enter(&*(sinhvien+i));
+        enter(sinhvien+i);
     }
 }
-void sapxep (SinhVien *sinhvien,int sosinhvien){
+void sapxep(SinhVien *sinhvien,int sosinhvien){
     for (int i = 0; i < sosinhvien;i++){
-        if ((sinhvien+i)->Diem < 5){
+        if((sinhvien+i)->Diem < 5){
             printf("Sinh vien co diem :%d\n",(sinhvien+i)->Diem);
             printf("Sinh vien diem kem co ma so sinh vien %d,ten: %s\n",(sinhvien+i)->Masosinhvien,(sinhvien+i)->Ten);
-        }else if ((sinhvien+i)->Diem > 5 || (sinhvien+i)->Diem < 6){
+        }else if((sinhvien+i)->Diem > 5 || (sinhvien+i)->Diem < 6){
             printf("Sinh vien co diem :%d\n",(sinhvien+i)->Diem);
             printf("Sinh vien diem yeu co ma so sinh vien %d,ten: %s\n",(sinhvien+i)->Masosinhvien,(sinhvien+i)->Ten); 
-        }else if ((sinhvien+i)->Diem > 6 || (sinhvien+i)->Diem < 7){
+        }else if((sinhvien+i)->Diem > 6 || (sinhvien+i)->Diem < 7){
             printf("Sinh vien co diem :%d\n",(sinhvien+i)->Diem);
             printf("Sinh vien diem trung binh co ma so sinh vien %d,ten: %s\n",(sinhvien+i)->Masosinhvien,(sinhvien+i)->Ten);
-        }else if  ((sinhvien+i)->Diem > 7 || (sinhvien+i)->Diem < 8){
+        }else if((sinhvien+i)->Diem > 7 || (sinhvien+i)->Diem < 8){
             printf("Sinh vien co diem :%d\n",(sinhvien+i)->Diem);
             printf("Sinh vien diem kha co ma so sinh vien %d,ten: %s\n",(sinhvien+i)->Masosinhvien,(sinhvien+i)->Ten);
-        }else if  ((sinhvien+i)->Diem > 8 || (sinhvien+i)->Diem < 9){
+        }else if((sinhvien+i)->Diem > 8 || (sinhvien+i)->Diem < 9){
             printf("Sinh vien co diem :%d\n",(sinhvien+i)->Diem);
             printf("Sinh vien diem gioi co ma so sinh vien %d,ten: %s\n",(sinhvien+i)->Masosinhvien,(sinhvien+i)->Ten);
         }
-        else {
+        else{
             printf("Sinh vien co diem :%d\n",(sinhvien+i)->Diem);
             printf("Sinh vien diem xuat sac co ma so sinh vien %d,ten: %s\n",(sinhvien+i)->Masosinhvien,(sinhvien+i)->Ten);
         }
